403c86ac64db2a0e954bbec6845da17ca8600f53
takuya kikuchi <t.kikuchi.pch@gmail.com>
2018-01-13 23:24:43 +0900

    Initial commit.

---
+++ LineBot/Controllers/MessagesController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using LineBot.Models;
+using LineMessagingAPISDK.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LineBot.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+//        // GET api/values
+//        [HttpGet]
+//        public IEnumerable<string> Get()
+//        {
+//            return new string[] {"value1", "value2"};
+//        }
+//        // GET api/messages/5
+//        [HttpGet("{id}")]
+//        public string Get(int id)
+//        {
+//            return "msg";
+//        }
+
+        // POST api/messages
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] string value)
+        {
+            var activity = JsonConvert.DeserializeObject<Activity>(value);
+
+            // Line may send multiple events in one message, so need to handle them all.
+            foreach (Event lineEvent in activity.Events)
+            {
+                LineMessageHandler handler = new LineMessageHandler(lineEvent);
+
+                Profile profile = await handler.GetProfile(lineEvent.Source.UserId);
+                //if(profile == null)
+                //{
+                //    return Request.CreateResponse(HttpStatusCode.OK);
+                //}
+                switch (lineEvent.Type)
+                {
+                    case EventType.Beacon:
+                        await handler.HandleBeaconEvent();
+                        break;
+                    case EventType.Follow:
+                        await handler.HandleFollowEvent();
+                        break;
+                    case EventType.Join:
+                        await handler.HandleJoinEvent();
+                        break;
+                    case EventType.Leave:
+                        await handler.HandleLeaveEvent();
+                        break;
+                    case EventType.Message:
+                        Message message = JsonConvert.DeserializeObject<Message>(lineEvent.Message.ToString());
+                        switch (message.Type)
+                        {
+                            case MessageType.Text:
+                                await handler.HandleTextMessage(MessageHandler.Current);
+                                break;
+                            case MessageType.Audio:
+                            case MessageType.Image:
+                            case MessageType.Video:
+                                await handler.HandleMediaMessage();
+                                break;
+                            case MessageType.Sticker:
+                                await handler.HandleStickerMessage();
+                                break;
+                            case MessageType.Location:
+                                await handler.HandleLocationMessage();
+                                break;
+                        }
+                        break;
+                    case EventType.Postback:
+                        await handler.HandlePostbackEvent();
+                        break;
+                    case EventType.Unfollow:
+                        await handler.HandleUnfollowEvent();
+                        break;
+                }
+            }
+
+            return this.Ok();
+
+        }
+    }
+}
---
+++ LineBot/Models/LineMessageHandler.cs
@@ -0,0 +1,112 @@
+﻿using System.Threading.Tasks;
+using LineMessagingAPISDK;
+using LineMessagingAPISDK.Models;
+using Microsoft.IdentityModel.Protocols;
+using Newtonsoft.Json;
+
+namespace LineBot.Models
+{
+    public class LineMessageHandler
+    {
+        private Event lineEvent;
+        private LineClient lineClient = new LineClient("DUMMY");
+
+        public LineMessageHandler(Event lineEvent)
+        {
+            this.lineEvent = lineEvent;
+        }
+
+        public async Task HandleBeaconEvent()
+        {
+        }
+
+        public async Task HandleFollowEvent()
+        {
+        }
+
+        public async Task HandleJoinEvent()
+        {
+        }
+
+        public async Task HandleLeaveEvent()
+        {
+        }
+
+        public async Task HandlePostbackEvent()
+        {
+            var replyMessage = new TextMessage(lineEvent.Postback.Data);
+            await Reply(replyMessage);
+        }
+
+        public async Task HandleUnfollowEvent()
+        {
+        }
+
+        public async Task<Profile> GetProfile(string mid)
+        {
+            return await lineClient.GetProfile(mid);
+        }
+
+        public async Task HandleTextMessage(MessageHandler handler)
+        {
+            var textMessage = JsonConvert.DeserializeObject<TextMessage>(lineEvent.Message.ToString());
+
+            var reply = handler.HandleTextMessage(lineEvent.Source.UserId + lineEvent.Source.GroupId, textMessage);
+
+            await Reply(new TextMessage(reply));
+        }
+
+
+
+        public async Task HandleMediaMessage()
+        {
+            Message message = JsonConvert.DeserializeObject<Message>(lineEvent.Message.ToString());
+            // Get media from Line server.
+            Media media = await lineClient.GetContent(message.Id);
+            Message replyMessage = null;
+
+            // Reply Image 
+            switch (message.Type)
+            {
+                case MessageType.Image:
+                case MessageType.Video:
+                case MessageType.Audio:
+                    replyMessage = new ImageMessage("https://github.com/apple-touch-icon.png", "https://github.com/apple-touch-icon.png");
+                    break;
+            }
+
+            await Reply(replyMessage);
+        }
+
+        public async Task HandleStickerMessage()
+        {
+            //https://devdocs.line.me/files/sticker_list.pdf
+            //var stickerMessage = JsonConvert.DeserializeObject<StickerMessage>(lineEvent.Message.ToString());
+            //var replyMessage = new StickerMessage("1", "1");
+            //await Reply(replyMessage);
+        }
+
+        public async Task HandleLocationMessage()
+        {
+            var locationMessage = JsonConvert.DeserializeObject<LocationMessage>(lineEvent.Message.ToString());
+            LocationMessage replyMessage = new LocationMessage(
+                locationMessage.Title,
+                locationMessage.Address,
+                locationMessage.Latitude,
+                locationMessage.Longitude);
+            await Reply(replyMessage);
+        }
+
+        private async Task Reply(Message replyMessage)
+        {
+            try
+            {
+                await lineClient.ReplyToActivityAsync(lineEvent.CreateReply(message: replyMessage));
+            }
+            catch
+            {
+                await lineClient.PushAsync(lineEvent.CreatePush(message: replyMessage));
+            }
+        }
+    }
+}
